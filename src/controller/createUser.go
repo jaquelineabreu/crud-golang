88b6051a5331ce1925d5d71aa497a6fa25315512
Jaqueline Abreu <59976892+jaquelineabreu@users.noreply.github.com>
2023-11-12 01:18:31 -0300

    feat: criação dos objetos user request e response

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,9 +1,24 @@
 package controller
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
+	"github.com/jaquelineabreu/crud-golang/src/configuration/rest_err"
+	"github.com/jaquelineabreu/crud-golang/src/controller/model/request"
 )
 
 func CreateUser(c *gin.Context) {
 
+	var userRequest request.UserRequest
+
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		restErr := rest_err.NewBadRequestError(
+			fmt.Sprintf("There are some incorrect filds, error=%s\n", err.Error()))
+
+		c.JSON(restErr.Code, restErr)
+		return
+	}
+
+	fmt.Println(userRequest)
 }
--- src/controller/routes/routes.go
+++ src/controller/routes/routes.go
@@ -6,7 +6,7 @@ import (
 )
 
 func InitRoutes(r *gin.RouterGroup) {
-
+	// relativepath	 			-  hadles
 	r.GET("/getUserById/:userId", controller.FindUserByID)
 	r.GET("/getUserByEmail/:userEmail", controller.FindUserByEmail)
 	r.POST("/createUser", controller.CreateUser)
